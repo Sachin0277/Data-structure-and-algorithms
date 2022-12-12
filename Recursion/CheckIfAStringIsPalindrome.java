d8188b0764140f7142edaf8dacab0a83e98edd50
Sachin Yadav <sachinyadav0278@gmail.com>
2022-12-12 22:59:52 +0530

    CheckWhetherTheGivenStringIsPalindromeOrNotUsingRecursion
    Implementation of check whether the given string is palindrome or not using recursion

---
+++ Recursion/CheckIfAStringIsPalindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CheckIfAStringIsPalindrome {
+    private static boolean palin(int i, String s){
+        if(i >= (s.length())/2)
+            return true;
+        if(s.charAt(i) != s.charAt(s.length()-i-1))
+            return false;
+       return palin(i+1,s);
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.psitrintln("Enter the string to check whether it is palindrome or not :");
+        String s = sc.nextLine();
+
+        if(palin(0,s))
+            System.out.println("Palindrome");
+        else 
+            System.out.println("Not palindrome");
+
+
+    }
+}
