fb70986fbfc206ec094322c6d7b17d8aedcad2a0
Sachin Yadav <sachinyadav0278@gmail.com>
2023-06-27 13:51:11 +0530

    codestudio-potd-6.27.2023
    Implentation of recursion

---
+++ CodeStudioPOTD/TowerOfHanoi.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class TowerOfHanoi {
+    private static void recur(int n, ArrayList<ArrayList<Integer>> ans, int src,int aux ,int dest){
+        if(n == 0) return;
+        recur(n-1,ans,src,dest,aux);
+        ArrayList<Integer> ls = new ArrayList<>();
+        ls.add(src);
+        ls.add(aux);
+        ans.add(ls);
+        recur(n-1,ans,dest,aux,src);
+    }
+
+    private static ArrayList<ArrayList<Integer>> towerOfHanoi(int n){
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        int src = 1, aux = 2,dest = 3;
+        recur(n,ans,src,aux,dest);
+        return ans;
+    }
+    public static void main(String args[]){
+        int n = 3;
+        ArrayList<ArrayList<Integer>> ans = towerOfHanoi(n);
+        for(ArrayList<Integer> l : ans){
+            System.out.println(l);
+        }
+    }
+}
