8ba89ed571e5e6e05355d2cce6d3146f3ed3430f
Sachin Yadav <sachinyadav0278@gmail.com>
2023-08-22 21:43:55 +0530

    Leetcode-POTD-8/22/2023
    Implemented using stringBuilder

---
+++ Leetcode and gfg practice/ExcelSheetColumnTitle.java
@@ -0,0 +1,17 @@
+public class ExcelSheetColumnTitle {
+    
+    private static String convertToTitle(int n){
+        StringBuilder stringBuilder = new StringBuilder();
+        while(n > 0){
+            n--;
+            stringBuilder.insert(0,(char)('A'+n%26));
+            n /= 26;
+        }
+        return stringBuilder.toString();
+    }
+    public static void main(String[] args) {
+        int n = 701;
+        System.out.println(convertToTitle(n));
+    }
+
+}
